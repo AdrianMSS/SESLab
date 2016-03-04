0c23365cfc83dafe0bf069266679c14f789b3e7d
AdrianMSS <sesamaua@gmail.com>
2016-03-04 10:34:50 -0600

    Commit #4 AulaVirtual Redirect

--- app.js
+++ app.js
@@ -14,6 +14,20 @@ app.use(bodyParser());
 app.use(express.static(__dirname + '/webpage'));
 
 
+app.get('/aulavirtual', function (req, res) {
+    res.writeHead(301,
+	  {Location: 'http://seslab.org/aulavirtual/'}
+	);
+	res.end();
+});
+
+app.get('/aulavirtual/*', function (req, res) {
+    res.writeHead(301,
+	  {Location: 'http://seslab.org/aulavirtual/'}
+	);
+	res.end();
+});
+
 app.get('*', function (req, res) {
     res.redirect('/index.html', 404);
 });
