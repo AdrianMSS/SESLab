cfb60c122976265eb98ed6ce7e73c4869e9e76e1
AdrianMSS <sesamaua@gmail.com>
2016-04-18 16:33:27 -0600

    Commit #8 MongoDB URI

--- services/database.js
+++ services/database.js
@@ -9,7 +9,7 @@ var mongo = require('mongodb'),
 // Here we find an appropriate database to connect to, defaulting to
 // localhost if we don't find one.  
 var uristring = 
-  process.env.MONGOLAB_URI || 
+  process.env.MONGODB_URI || 
   process.env.MONGOHQ_URL || 
   'mongodb://localhost/SESLab-Curso';
 
